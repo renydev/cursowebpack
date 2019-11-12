fcc815f82851714f2fbfde89f325e3ea0738a1fa
Renato Ventura <re.infotec@gmail.com>
2019-11-12 19:03:39 -0300

    Usando Plugins
    Este episódio ensina a usar o plugin 'html-webpack-plugin', separar um template e gerar um index na pasta dist.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
@@ -11,7 +12,10 @@ module.exports = {
     app: `${PATHS.src}/index2.js`,
   },
   output: {
-    filename: '[name].js', // [name] isso é um placeholder, serve como variável
+    filename: '[name].[chunkhash].js', // Como o sistema está criando meu index.html programaticamente, 
+    // eu posso exigir um hash que ele será inserido no html automaticamente
+
+    // filename: '[name].js', // [name] isso é um placeholder, serve como variável
     // filename: '[name].[hash].js', // [name] isso é um placeholder, serve como variável
     // a variável [hash] serve justamente para criar um hash e o navegador parar de cachear o arquivo
     // a variável [chunkhash] gera um hash diferente somente se o arquivo foi alterado 
@@ -41,6 +45,13 @@ module.exports = {
 
     ]
   },
+  plugins: [
+    /* Todos os plugins devem ser estanciados dentro do atributo de plugins */
+    new HtmlWebpackPlugin({
+      title: 'TW Webpack',
+      template: `${PATHS.src}/index.html`,// é necessário indicar um template para que o sistema gere algum html e exiba a imagem
+    })
+  ],
   optimization: {
     minimize: true
     /*
